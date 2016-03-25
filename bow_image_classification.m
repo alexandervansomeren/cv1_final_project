166d46932995a2d4f0edd8dd064cbd7a9ecf4fba
Alexander <Alex@woodl.nl>
2016-03-25 12:12:46 +0100

    Prediction is not working

--- bow_image_classification.m
+++ bow_image_classification.m
@@ -6,10 +6,10 @@ kmeans_image_sample_index = 1:N;
 svm_image_sample_index = N+1:2*N;
 k_means_sample_size = 10000;
 image_data_path = 'Caltech4/ImageData/';
-classes_path = strcat('classes_sample_', num2str(N),'.mat');
-centroids_path = strcat('debug_centroids_sample_', num2str(N),'.mat');
-vocabulary_size = 400;
 color_space = 'd_RGB'; % {'d_RGB';'d_rgb';'d_gray';'d_opponent';'d_RGB_dense';..}
+classes_path = strcat('classes_sample_', num2str(N),'.mat');
+centroids_path = strcat('debug_centroids_sample_', num2str(N),'_',color_space,'.mat');
+vocabulary_size = 400;
 fast_kmeans = false;
 dense_sift_step_size = 15;
 
@@ -23,13 +23,13 @@ else
     
     classes = struct([]);
     for current_class = 1:length(class_names)
-        image_files = dir(strcat(image_data_path, class_names{current_class}, '_train/'));
+        image_files = dir(strcat(image_data_path, class_names{current_class}, '_train/*jpg'));
         image_files = {image_files.name};
-        image_files(1:2) = []; % remove first junk files
+        %         image_files(1:2) = []; % remove first junk files
         
-        image_files_test = dir(strcat(image_data_path, class_names{current_class}, '_test/'));
+        image_files_test = dir(strcat(image_data_path, class_names{current_class}, '_test/*jpg'));
         image_files_test = {image_files_test.name};
-        image_files_test(1:2) = []; % remove first junk files
+        %         image_files_test(1:2) = []; % remove first junk files
         
         % Sample for kmeans and svm
         sample = randsample(image_files, 2*N, false);
@@ -48,6 +48,7 @@ else
             im_path = strcat(image_data_path, ...
                 class_names{current_class},'_train/', ...
                 classes(current_class).image_files_sample{i});
+            disp(im_path);
             image_RGB = imread(im_path);
             image_rgb = RGB2rgb(image_RGB);
             image_gray = rgb2gray(image_RGB);
@@ -61,20 +62,20 @@ else
             [~, d_RGB.R] = vl_sift(im2single(image_RGB(:,:,1)));
             [~, d_RGB.G] = vl_sift(im2single(image_RGB(:,:,2)));
             [~, d_RGB.B] = vl_sift(im2single(image_RGB(:,:,3)));
-%             d_RGB_dense = {};
-%             [~, d_RGB_dense.R] = vl_dsift(im2single(image_RGB(:,:,1)), 'size', dense_sift_bin_size);
-%             [~, d_RGB_dense.G] = vl_dsift(im2single(image_RGB(:,:,2)), 'size', dense_sift_bin_size);
-%             [~, d_RGB_dense.B] = vl_dsift(im2single(image_RGB(:,:,3)), 'size', dense_sift_bin_size);
+            %             d_RGB_dense = {};
+            %             [~, d_RGB_dense.R] = vl_dsift(im2single(image_RGB(:,:,1)), 'size', dense_sift_bin_size);
+            %             [~, d_RGB_dense.G] = vl_dsift(im2single(image_RGB(:,:,2)), 'size', dense_sift_bin_size);
+            %             [~, d_RGB_dense.B] = vl_dsift(im2single(image_RGB(:,:,3)), 'size', dense_sift_bin_size);
             
             % rgb
             d_rgb = {};
             [~, d_rgb.R] = vl_sift(im2single(image_rgb(:,:,1)));
             [~, d_rgb.G] = vl_sift(im2single(image_rgb(:,:,2)));
             [~, d_rgb.B] = vl_sift(im2single(image_rgb(:,:,3)));
-%             d_rgb_dense = {};
-%             [~, d_rgb_dense.R] = vl_dsift(im2single(image_rgb(:,:,1)), 'size', dense_sift_bin_size);
-%             [~, d_rgb_dense.G] = vl_dsift(im2single(image_rgb(:,:,2)), 'size', dense_sift_bin_size);
-%             [~, d_rgb_dense.B] = vl_dsift(im2single(image_rgb(:,:,3)), 'size', dense_sift_bin_size);
+            %             d_rgb_dense = {};
+            %             [~, d_rgb_dense.R] = vl_dsift(im2single(image_rgb(:,:,1)), 'size', dense_sift_bin_size);
+            %             [~, d_rgb_dense.G] = vl_dsift(im2single(image_rgb(:,:,2)), 'size', dense_sift_bin_size);
+            %             [~, d_rgb_dense.B] = vl_dsift(im2single(image_rgb(:,:,3)), 'size', dense_sift_bin_size);
             
             % gray
             d_gray = {};
@@ -87,21 +88,21 @@ else
             [~, d_opponent.R] = vl_sift(im2single(image_opponent(:,:,1)));
             [~, d_opponent.G] = vl_sift(im2single(image_opponent(:,:,2)));
             [~, d_opponent.B] = vl_sift(im2single(image_opponent(:,:,3)));
-%             d_opponent_dense = {};
-%             [~, d_opponent_dense.R] = vl_dsift(im2single(image_opponent(:,:,1)), 'size', dense_sift_bin_size);
-%             [~, d_opponent_dense.G] = vl_dsift(im2single(image_opponent(:,:,2)), 'size', dense_sift_bin_size);
-%             [~, d_opponent_dense.B] = vl_dsift(im2single(image_opponent(:,:,3)), 'size', dense_sift_bin_size);
+            %             d_opponent_dense = {};
+            %             [~, d_opponent_dense.R] = vl_dsift(im2single(image_opponent(:,:,1)), 'size', dense_sift_bin_size);
+            %             [~, d_opponent_dense.G] = vl_dsift(im2single(image_opponent(:,:,2)), 'size', dense_sift_bin_size);
+            %             [~, d_opponent_dense.B] = vl_dsift(im2single(image_opponent(:,:,3)), 'size', dense_sift_bin_size);
             
             
             % add descriptors to the data model
             classes(current_class).image_samples(i).d_RGB = d_RGB;
-%             classes(current_class).image_samples(i).d_RGB_dense = d_RGB_dense;
+            %             classes(current_class).image_samples(i).d_RGB_dense = d_RGB_dense;
             classes(current_class).image_samples(i).d_rgb = d_rgb;
-%             classes(current_class).image_samples(i).d_rgb_dense = d_rgb_dense;
+            %             classes(current_class).image_samples(i).d_rgb_dense = d_rgb_dense;
             classes(current_class).image_samples(i).d_gray = d_gray;
             classes(current_class).image_samples(i).d_gray_dense = d_gray_dense;
             classes(current_class).image_samples(i).d_opponent = d_opponent;
-%             classes(current_class).image_samples(i).d_opponent_dense = d_opponent_dense;
+            %             classes(current_class).image_samples(i).d_opponent_dense = d_opponent_dense;
         end
         classes(current_class).images_test = struct([]);
         for i = 1:length(classes(current_class).image_files_test)
@@ -121,20 +122,20 @@ else
             [~, d_RGB.R] = vl_sift(im2single(image_RGB(:,:,1)));
             [~, d_RGB.G] = vl_sift(im2single(image_RGB(:,:,2)));
             [~, d_RGB.B] = vl_sift(im2single(image_RGB(:,:,3)));
-%             d_RGB_dense = {};
-%             [~, d_RGB_dense.R] = vl_dsift(im2single(image_RGB(:,:,1)), 'size', dense_sift_bin_size);
-%             [~, d_RGB_dense.G] = vl_dsift(im2single(image_RGB(:,:,2)), 'size', dense_sift_bin_size);
-%             [~, d_RGB_dense.B] = vl_dsift(im2single(image_RGB(:,:,3)), 'size', dense_sift_bin_size);
+            %             d_RGB_dense = {};
+            %             [~, d_RGB_dense.R] = vl_dsift(im2single(image_RGB(:,:,1)), 'size', dense_sift_bin_size);
+            %             [~, d_RGB_dense.G] = vl_dsift(im2single(image_RGB(:,:,2)), 'size', dense_sift_bin_size);
+            %             [~, d_RGB_dense.B] = vl_dsift(im2single(image_RGB(:,:,3)), 'size', dense_sift_bin_size);
             
             % rgb
             d_rgb = {};
             [~, d_rgb.R] = vl_sift(im2single(image_rgb(:,:,1)));
             [~, d_rgb.G] = vl_sift(im2single(image_rgb(:,:,2)));
             [~, d_rgb.B] = vl_sift(im2single(image_rgb(:,:,3)));
-%             d_rgb_dense = {};
-%             [~, d_rgb_dense.R] = vl_dsift(im2single(image_rgb(:,:,1)), 'size', dense_sift_bin_size);
-%             [~, d_rgb_dense.G] = vl_dsift(im2single(image_rgb(:,:,2)), 'size', dense_sift_bin_size);
-%             [~, d_rgb_dense.B] = vl_dsift(im2single(image_rgb(:,:,3)), 'size', dense_sift_bin_size);
+            %             d_rgb_dense = {};
+            %             [~, d_rgb_dense.R] = vl_dsift(im2single(image_rgb(:,:,1)), 'size', dense_sift_bin_size);
+            %             [~, d_rgb_dense.G] = vl_dsift(im2single(image_rgb(:,:,2)), 'size', dense_sift_bin_size);
+            %             [~, d_rgb_dense.B] = vl_dsift(im2single(image_rgb(:,:,3)), 'size', dense_sift_bin_size);
             
             % gray
             d_gray = {};
@@ -149,21 +150,21 @@ else
             [~, d_opponent.R] = vl_sift(im2single(image_opponent(:,:,1)));
             [~, d_opponent.G] = vl_sift(im2single(image_opponent(:,:,2)));
             [~, d_opponent.B] = vl_sift(im2single(image_opponent(:,:,3)));
-%             d_opponent_dense = {};
-%             [~, d_opponent_dense.R] = vl_dsift(im2single(image_opponent(:,:,1)), 'size', dense_sift_bin_size);
-%             [~, d_opponent_dense.G] = vl_dsift(im2single(image_opponent(:,:,2)), 'size', dense_sift_bin_size);
-%             [~, d_opponent_dense.B] = vl_dsift(im2single(image_opponent(:,:,3)), 'size', dense_sift_bin_size);
+            %             d_opponent_dense = {};
+            %             [~, d_opponent_dense.R] = vl_dsift(im2single(image_opponent(:,:,1)), 'size', dense_sift_bin_size);
+            %             [~, d_opponent_dense.G] = vl_dsift(im2single(image_opponent(:,:,2)), 'size', dense_sift_bin_size);
+            %             [~, d_opponent_dense.B] = vl_dsift(im2single(image_opponent(:,:,3)), 'size', dense_sift_bin_size);
             
             
             % add descriptors to the data model
-            classes(current_class).image_test(i).d_RGB = d_RGB;
-%             classes(current_class).image_test(i).d_RGB_dense = d_RGB_dense;
-            classes(current_class).image_test(i).d_rgb = d_rgb;
-%             classes(current_class).image_test(i).d_rgb_dense = d_rgb_dense;
-            classes(current_class).image_test(i).d_gray = d_gray;
-            classes(current_class).image_test(i).d_gray_dense = d_gray_dense;
-            classes(current_class).image_test(i).d_opponent = d_opponent;
-%             classes(current_class).image_test(i).d_opponent_dense = d_opponent_dense;
+            classes(current_class).images_test(i).d_RGB = d_RGB;
+            %             classes(current_class).images_test(i).d_RGB_dense = d_RGB_dense;
+            classes(current_class).images_test(i).d_rgb = d_rgb;
+            %             classes(current_class).images_test(i).d_rgb_dense = d_rgb_dense;
+            classes(current_class).images_test(i).d_gray = d_gray;
+            classes(current_class).images_test(i).d_gray_dense = d_gray_dense;
+            classes(current_class).images_test(i).d_opponent = d_opponent;
+            %             classes(current_class).images_test(i).d_opponent_dense = d_opponent_dense;
         end
     end
     classes = rmfield(classes, 'image_files');
@@ -252,52 +253,66 @@ save('debug_classes_hist.mat', 'classes');
 
 %% Classification
 
-% Train models
-for current_class_svm = 1:length(classes)
-    % Create trainset table
-    svm_train_data = array2table(zeros(length(classes)*N,vocabulary_size+1));
-    svm_train_data.Properties.VariableNames(vocabulary_size+1) = {'class'};
-    d_index = 1;
-    % Retrieve histograms
-    for current_class = 1:length(classes)
-        if strcmp(classes(current_class).name, classes(current_class_svm).name)
-            c = 1;
-        else
-            c = 2;
+if exist('debug_svm_models.mat', 'file')
+    load('debug_svm_models.mat')
+else
+    % Train models
+    for current_class_svm = 1:length(classes)
+        % Create trainset table
+        svm_train_data = array2table(zeros(length(classes)*N,vocabulary_size+1));
+        svm_train_data.Properties.VariableNames(vocabulary_size+1) = {'class'};
+        d_index = 1;
+        % Retrieve histograms
+        for current_class = 1:length(classes)
+            if strcmp(classes(current_class).name, classes(current_class_svm).name)
+                c = 1;
+            else
+                c = 2;
+            end
+            for i = 1:length(classes(current_class).image_samples(svm_image_sample_index)) % images
+                svm_train_data(d_index,1:vocabulary_size) = ...
+                    array2table(classes(current_class).image_samples(i).histogram);
+                svm_train_data(d_index,vocabulary_size+1) = {c};
+                d_index = d_index + 1;
+            end
         end
-        for i = 1:length(classes(current_class).image_samples(svm_image_sample_index)) % images
-            svm_train_data(d_index,1:vocabulary_size) = ...
-                array2table(classes(current_class).image_samples(i).histogram);
-            svm_train_data(d_index,vocabulary_size+1) = {c};
-            d_index = d_index + 1;
-        end
+        classes(current_class_svm).svm_model = fitcsvm(svm_train_data, 'class', ...
+            'KernelFunction', 'rbf');
     end
-    classes(current_class_svm).svm_model = fitcsvm(svm_train_data, 'class');
+    
+    save('debug_svm_models.mat', 'classes');
 end
-
-save('debug_svm_models.mat', 'classes');
-
 %% Evaluation
 for eval_class = 1:length(classes)
-    hists_to_predict = zeros(length(classes(current_class).images_test)* ...
-                                length(classes), vocabulary_size);
+    hists_to_predict = array2table(zeros(length(classes(current_class).images_test)* ...
+        length(classes), vocabulary_size)); % +1 for im_path
+%     hists_to_predict.Properties.VariableNames(vocabulary_size+1) = {'class'};
+    %     hists_to_predict.Properties.VariableNames(vocabulary_size+2) = {'im_path'};
+    d_index = 1;
     for current_class = 1:length(classes)
         for i = 1:length(classes(current_class).images_test)  % images
             image_descriptors = cell([1 length(spaces)*3]);
-            for j = 1:length(spaces);
-                channels = fieldnames(classes(current_class).images_test(i).(spaces{j}));
-                for k = 1:length(channels)
-                    descriptor = ...
-                        classes(current_class).image_samples(i).(spaces{j}).(channels{k});
-                    image_descriptors{d} = descriptor;
-                end
+            channels = fieldnames(classes(current_class).images_test(i).(color_space));
+            d=1;
+            for k = 1:length(channels)
+                descriptor = ...
+                    classes(current_class).images_test(i).(color_space).(channels{k});
+                image_descriptors{d} = descriptor;
+                d = d+1;
             end
             image_descriptors = image_descriptors(~cellfun('isempty', image_descriptors));
-            hists_to_predict(i,:) = getHistFromDescriptor(double(cell2mat(image_descriptors)'), centroids);
+            warning('off','all');
+            hists_to_predict(d_index,1:vocabulary_size) = array2table(getHistFromDescriptor(double(cell2mat(image_descriptors)'), centroids));
+            warning('on','all');
+            d_index = d_index+1;
+%             hists_to_predict(d_index,vocabulary_size+1) = {current_class};
+            %             hists_to_predict(d_index,vocabulary_size+2) = classes(current_class).images_test(i).im_path;
         end
     end
-    [label,Score] = classes(eval_class).svm_model.Methods.predict(hists_to_predict);
+    [label,Score] = predict(classes(eval_class).svm_model, hists_to_predict);
 end
+
+save('debug_all_files_after_eval');
 
 end
 
@@ -307,8 +322,8 @@ end
 
 % Helper function that creates the histogram given a matrix of descriptors
 % and a matrix of centroids
-function h = getHistFromDescriptor(descriptors, centroids)
-D = pdist2(descriptors,centroids);
-[~,I] = min(D, [], 2);
-h = hist(I, size(centroids,1));
-end
+% function h = getHistFromDescriptor(descriptors, centroids)
+% D = pdist2(descriptors,centroids);
+% [~,I] = min(D, [], 2);
+% h = hist(I, size(centroids,1));
+% end
